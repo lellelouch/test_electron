b7bbe7a31980807ac2a05d1f618592ae916d8713
En Taro Michael <lelouchrebellion98@yahoo.com>
2017-08-31 17:34:23 +0800

    updates

--- main.js
+++ main.js
@@ -13,11 +13,11 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 800, height: 600})
-
+  mainWindow = new BrowserWindow({width: 800, height: 600, icon: path.join(__dirname, 'app/iconforapp.png'), frame: false, minWidth: 500, minHeight: 500})
+  
   // and load the index.html of the app.
   mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
+    pathname: path.join(__dirname, 'app/index.html'),
     protocol: 'file:',
     slashes: true
   }))
